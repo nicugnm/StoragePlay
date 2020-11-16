0f52be35171ad495fd57fa7a1bba34a9ba6606d8
nicugnm <nicolae.ghergu@s.unibuc.ro>
2020-11-16 22:19:52 +0200

    first commit

---
+++ src/main/java/com/nicugnm/storageplay/StoragePlayApplication.java
@@ -0,0 +1,13 @@
+package com.nicugnm.storageplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoragePlayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoragePlayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nicugnm/storageplay/StoragePlayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicugnm.storageplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoragePlayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
